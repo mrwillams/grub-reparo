77100b093e76f50df99c15d1fc6bc8187d818ddf
mrwillams <sonhadodouniverso@gmail.com>
2016-02-08 18:56:22 -0300

    Create grub_reparo.sh

---
+++ grub_reparo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "$(tput setaf 3)--- Este Script Foi Criado Pelo Mr. Willams Com Intensão De Facilita o Reparo do Grub ---"
+
+sleep 2
+
+read -p "Pressione Enter para continuar, ou CTRL+C para cancelar" nothing
+
+sudo mount -t ext4 /dev/sda3 /mnt
+
+sudo grub-install --root-directory=/mnt /dev/sda 
+
+sudo update-grub
